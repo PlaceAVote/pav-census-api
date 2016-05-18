ebfff4fe0716d2c33a6fea9951306ce6bd4df46a
HistoireDeBabar <p.barber1990@gmail.com>
2016-05-18 01:09:57 +0100

    remove db creds

---
+++ scripts/compile_config.js
@@ -0,0 +1,16 @@
+const mustache = require('mustache');
+const fs = require('fs');
+
+const host = process.argv[process.argv.length-3];
+const user = process.argv[process.argv.length-2];
+const password = process.argv[process.argv.length-1];
+
+const config = {
+  host: host,
+  user: user,
+  password: password,
+};
+
+const template = fs.readFileSync(`${__dirname}/../lib/config/config.mustache`, {encoding: 'utf-8'});
+const output = mustache.render(template, config);
+fs.writeFileSync(`${__dirname}/../lib/config/config.js`, output);
