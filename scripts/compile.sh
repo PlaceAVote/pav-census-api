e508cbf4796eb16c9718547ea8c17428a8db88bc
HistoireDeBabar <p.barber1990@gmail.com>
2016-05-20 00:54:06 +0100

    update compile temp scripts to include new data connetion settings

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,6 +6,11 @@ HOST=${DEV_HOST}
 USER=${DEV_USER}
 PASSWORD=${DEV_PASSWORD}
 
+USER_HOST=${DEV_USER_HOST}
+USER_USER=${DEV_USER_USER}
+USER_PASSWORD=${DEV_USER_PASSWORD}
+USER_DB=${DEV_USER_DB}
+
 echo "Compiling Config"
-cd ./scripts && node ./compile_config.js ${HOST} ${USER} ${PASSWORD}
+node ./scripts/compile_config.js ${HOST} ${USER} ${PASSWORD} ${USER_HOST} ${USER_USER} ${USER_PASSWORD} ${USER_DB}
 echo "Config Compiled"
--- scripts/compile_config.js
+++ scripts/compile_config.js
@@ -1,16 +1,23 @@
 const mustache = require('mustache');
 const fs = require('fs');
 
-const host = process.argv[process.argv.length-3];
-const user = process.argv[process.argv.length-2];
-const password = process.argv[process.argv.length-1];
 
-console.log(`Setting Host ${host}, ${user} and ${password}`);
-
+const host = process.argv[process.argv.length-7];
+const user = process.argv[process.argv.length-6];
+const password = process.argv[process.argv.length-5];
+const user_host = process.argv[process.argv.length-4];
+const user_user = process.argv[process.argv.length-3];
+const user_password = process.argv[process.argv.length-2];
+const user_database = process.argv[process.argv.length-1];
+console.log(`Setting Host ${host}, ${user} and ${password}. Setting User Host ${user_host}, ${user_user}, ${user_password} and ${user_database}`);
 const config = {
   host: host,
   user: user,
   password: password,
+  user_host: user_host,
+  user_user: user_user,
+  user_password: user_password,
+  user_database: user_database,
 };
 
 const template = fs.readFileSync(`${__dirname}/../lib/config/config.mustache`, {encoding: 'utf-8'});
