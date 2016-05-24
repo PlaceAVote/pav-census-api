fa739e982623ac534dc9f5d601e3af2e492531af
HistoireDeBabar <p.barber1990@gmail.com>
2016-05-24 17:11:11 +0100

    add Autherization to allowed headers

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(applicationLogger('combined'));
 
 const cors = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 };
 
